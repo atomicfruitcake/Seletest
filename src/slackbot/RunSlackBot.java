9519353ceb539d2128085a7409261c51e141e3f7
atomicfruitcake <samjbass@hotmail.co.uk>
2016-11-24 22:34:40 +0000

    Update RunSlackBot.java

--- src/slackbot/RunSlackBot.java
+++ src/slackbot/RunSlackBot.java
@@ -2,15 +2,12 @@ package slackbot;
 
 import java.io.IOException;
 
-import org.testng.annotations.Test;
-
 /**
  * @author sambass
  *
  */
-@Test
 public class RunSlackBot {
-	public void slackbotTest() throws IOException {
+	public static void main(String args[]) throws IOException {
 		SlackBot.slackSession();
 		SlackBot.registerAListener();		
 		while (true) {
